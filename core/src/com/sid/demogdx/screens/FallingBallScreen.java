4d7bad72709d50f3300d8b9ad0eb887caaa347a0
Rimas G <rimasg@gmail.com>
2016-03-19 20:08:19 +0200

    Updates

--- core/src/com/sid/demogdx/screens/FallingBallScreen.java
+++ core/src/com/sid/demogdx/screens/FallingBallScreen.java
@@ -4,15 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.sid.demogdx.Assets;
 import com.sid.demogdx.DemoGdx;
 import com.sid.demogdx.utils.AppConfig;
 import com.sid.demogdx.utils.Box2dUtils;
@@ -32,7 +37,9 @@ public class FallingBallScreen extends AbstractScreen {
     OrthogonalTiledMapRenderer mapRenderer;
     Box2DMapObjectParser box2DMapObjectParser;
 
-    Body ball;
+    private Body ball;
+    private Array<Body> bodies = new Array<Body>();
+    private TextureAtlas.AtlasRegion ballRegion;
 
     public FallingBallScreen(DemoGdx game) {
         super(game);
@@ -43,21 +50,28 @@ public class FallingBallScreen extends AbstractScreen {
         Gdx.input.setCatchBackKey(true);
         viewport = new FitViewport(AppConfig.WORLD_WIDTH_VIRTUAL, AppConfig.WORLD_HEIGHT_VIRTUAL, cam);
         viewport.apply(true);
-        world = new World(new Vector2(0, -9.8f), true);
+        world = new World(new Vector2(0, -4.9f), true);
         b2dr = new Box2DDebugRenderer();
         //
         map = new TmxMapLoader().load("maps/map.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map, AppConfig.unitScale32, game.batch);
         box2DMapObjectParser = new Box2DMapObjectParser(mapRenderer.getUnitScale());
         //
+        loadAssets();
+        //
         createBall();
         createWorld();
+    }
+
+    private void loadAssets() {
+        final Skin skin = Assets.inst().get("skin.json", Skin.class);
+        ballRegion = skin.getAtlas().findRegion("star");
     }
 
     private void createBall() {
         ball = Box2dUtils.createBox2dCircleBody(world,
                 AppConfig.WORLD_WIDTH_VIRTUAL / 2,
-                AppConfig.WORLD_HEIGHT_VIRTUAL * 2.0f);
+                AppConfig.WORLD_HEIGHT_VIRTUAL * 1.5f);
     }
 
     private void createWorld() {
@@ -74,10 +88,15 @@ public class FallingBallScreen extends AbstractScreen {
         world.step(delta, 6, 2);
         cam.position.set(viewport.getWorldWidth() / 2, ball.getPosition().y, 0);
         cam.update();
+        game.batch.setProjectionMatrix(cam.combined);
         //
         b2dr.render(world, cam.combined);
         mapRenderer.setView(cam);
         mapRenderer.render();
+        game.batch.begin();
+        drawBall();
+        drawBodies();
+        game.batch.end();
     }
 
     private void handleInput() {
@@ -87,6 +106,33 @@ public class FallingBallScreen extends AbstractScreen {
 
         if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
             game.setScreen(game.getMainMenuScreen());
+        }
+    }
+
+    private void drawBall() {
+        final float radius = ball.getFixtureList().get(0).getShape().getRadius();
+        game.batch.draw(ballRegion,
+                ball.getPosition().x - radius,
+                ball.getPosition().y - radius,
+                radius,
+                radius,
+                radius * 2, radius * 2, 1, 1,
+                ball.getAngle() * MathUtils.radiansToDegrees);
+    }
+
+    private void drawBodies() {
+        world.getBodies(bodies);
+        for (Body body : bodies) {
+            if ((body.getUserData() != null) && ((String) body.getUserData()).equals("HangingCircle")) {
+                final float radius = body.getFixtureList().get(0).getShape().getRadius();
+                game.batch.draw(ballRegion,
+                        body.getPosition().x,
+                        body.getPosition().y,
+                        radius,
+                        radius,
+                        radius * 2, radius * 2, 1, 1,
+                        body.getAngle() * MathUtils.radiansToDegrees);
+            }
         }
     }
 
