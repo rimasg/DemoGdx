e3e486add4b67e662faa85f34ca6b534d944ca92
Rimas G <rimasg@gmail.com>
2016-12-01 18:21:46 +0200

    AssetPacker added

---
+++ desktop/src/com/sid/demogdx/desktop/AssetPacker.java
@@ -0,0 +1,19 @@
+package com.sid.demogdx.desktop;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+/**
+ * Created by SID on 2016-12-01.
+ */
+
+public class AssetPacker {
+    private static final String RAW_ASSET_PATH = "desktop/assets-raw/textures";
+    private static final String ASSET_PATH = "android/assets/textures";
+
+    // TODO: 2016-12-01 copy assets file to "assests-raw" and check if working
+    public static void main(String[] args) {
+        final TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.atlasExtension = ".pack";
+        TexturePacker.process(settings, RAW_ASSET_PATH, ASSET_PATH, "texture");
+    }
+}
