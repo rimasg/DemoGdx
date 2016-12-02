0a2219467d2911c5f9dfabd54f8ef79dda1df1ef
Rimas G <rimasg@gmail.com>
2016-12-02 08:48:43 +0200

    AssetPacker modified

--- desktop/src/com/sid/demogdx/desktop/AssetPacker.java
+++ desktop/src/com/sid/demogdx/desktop/AssetPacker.java
@@ -10,7 +10,6 @@ public class AssetPacker {
     private static final String RAW_ASSET_PATH = "desktop/assets-raw/textures";
     private static final String ASSET_PATH = "android/assets/textures";
 
-    // TODO: 2016-12-01 copy assets file to "assets-raw" and check if working
     public static void main(String[] args) {
         final TexturePacker.Settings settings = new TexturePacker.Settings();
         settings.atlasExtension = ".pack";
