9fecbed469205c24f51b18948805a118a0b4c985
Rimas G. <rimasg@gmail.com>
2023-11-21 21:27:59 +0200

    feat: added StackTowerScreen#createBlocks

--- core/src/com/sid/demogdx/screens/StackTowerScreen.java
+++ core/src/com/sid/demogdx/screens/StackTowerScreen.java
@@ -1,5 +1,11 @@
 package com.sid.demogdx.screens;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
 import com.sid.demogdx.DemoGdx;
 import com.sid.demogdx.utils.Box2DConfig;
 import com.sid.demogdx.utils.Box2DUtils;
@@ -9,6 +15,10 @@ public class StackTowerScreen extends AbstractBox2dScreen {
         super(game);
     }
 
+    private final Array<Body> bodies = new Array<>();
+    private final float defaultBoxWidth = 2.0f;
+    private final float defaultBoxHeight = 0.5f;
+
     @Override
     public void render(float delta) {
         super.render(delta);
@@ -16,10 +26,24 @@ public class StackTowerScreen extends AbstractBox2dScreen {
         cam.update();
         b2dr.render(world, cam.combined);
 
+        game.shapeRenderer.setProjectionMatrix(cam.combined);
+        game.shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        renderBodyShapes();
+        game.shapeRenderer.end();
+
+
         game.batch.setProjectionMatrix(cam.combined);
         game.batch.begin();
         // TODO: 20/11/2023 Draw something
         game.batch.end();
+    }
+
+    private void renderBodyShapes() {
+        game.shapeRenderer.setColor(Color.RED);
+        for (Body body : bodies) {
+            final Vector2 pos = body.getPosition();
+            game.shapeRenderer.circle(pos.x, pos.y, 0.2f);
+        }
     }
 
     @Override
@@ -32,10 +56,23 @@ public class StackTowerScreen extends AbstractBox2dScreen {
         super.init();
 
         Box2DUtils.createWorldBoundaries(world);
-        createBlock();
+        createBlocks();
     }
 
-    private void createBlock() {
-        Box2DUtils.createBox2dPolygonBody(world, Box2DConfig.WWV / 2.0f, Box2DConfig.WHV * 0.8f);
+    private void createBlocks() {
+        for (int i = 0; i < 5; i++) {
+            final Body body = Box2DUtils.createBox2dBoxBody(world,
+                    Box2DConfig.WWV / 2.0f + MathUtils.random(0.1f * defaultBoxWidth, 0.4f * defaultBoxWidth),
+                    Box2DConfig.WHV * 0.4f + i * defaultBoxHeight * 2.0f * 1.1f,
+                    defaultBoxWidth, defaultBoxHeight);
+            bodies.add(body);
+        }
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+
+        bodies.clear();
     }
 }
