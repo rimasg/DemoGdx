a770f5bd447e57e22b0fdaa60738db09a9a6793c
Rimas G. <rimasg@gmail.com>
2023-11-27 21:22:24 +0200

    create upperBox and lowerBox boxes

--- core/src/com/sid/demogdx/screens/StackTowerScreen.java
+++ core/src/com/sid/demogdx/screens/StackTowerScreen.java
@@ -3,9 +3,7 @@ package com.sid.demogdx.screens;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.utils.Array;
 import com.sid.demogdx.DemoGdx;
 import com.sid.demogdx.utils.Box2DConfig;
 import com.sid.demogdx.utils.Box2DUtils;
@@ -15,7 +13,8 @@ public class StackTowerScreen extends AbstractBox2dScreen {
         super(game);
     }
 
-    private final Array<Body> bodies = new Array<>();
+    private Body upperBox;
+    private Body lowerBox;
     private final float defaultBoxWidth = 2.0f;
     private final float defaultBoxHeight = 0.5f;
 
@@ -40,10 +39,8 @@ public class StackTowerScreen extends AbstractBox2dScreen {
 
     private void renderBodyShapes() {
         game.shapeRenderer.setColor(Color.RED);
-        for (Body body : bodies) {
-            final Vector2 pos = body.getPosition();
-            game.shapeRenderer.circle(pos.x, pos.y, 0.2f);
-        }
+        game.shapeRenderer.circle(upperBox.getPosition().x, upperBox.getPosition().y, 0.2f);
+        game.shapeRenderer.circle(lowerBox.getPosition().x, lowerBox.getPosition().y, 0.2f);
     }
 
     @Override
@@ -60,19 +57,14 @@ public class StackTowerScreen extends AbstractBox2dScreen {
     }
 
     private void createBlocks() {
-        for (int i = 0; i < 5; i++) {
-            final Body body = Box2DUtils.createBox2dBoxBody(world,
-                    Box2DConfig.WWV / 2.0f + MathUtils.random(0.1f * defaultBoxWidth, 0.4f * defaultBoxWidth),
-                    Box2DConfig.WHV * 0.4f + i * defaultBoxHeight * 2.0f * 1.1f,
-                    defaultBoxWidth, defaultBoxHeight);
-            bodies.add(body);
-        }
+        upperBox = Box2DUtils.createBox2dBoxBody(world,
+                Box2DConfig.WWV / 2.0f + MathUtils.random(-0.4f * defaultBoxWidth, 0.4f * defaultBoxWidth),
+                Box2DConfig.WHV * 0.2f + 1 * defaultBoxHeight * 2.0f * 1.1f,
+                defaultBoxWidth, defaultBoxHeight);
+        lowerBox = Box2DUtils.createBox2dBoxBody(world,
+                Box2DConfig.WWV / 2.0f,
+                Box2DConfig.WHV * 0.2f + 0 * defaultBoxHeight * 2.0f * 1.1f,
+                defaultBoxWidth, defaultBoxHeight);
     }
 
-    @Override
-    public void hide() {
-        super.hide();
-
-        bodies.clear();
-    }
 }
